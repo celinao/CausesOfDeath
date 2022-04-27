d4e899a358ff14700b383253d9674daa1c84eb61
Celina Ough <ough@wisc.edu>
2022-04-27 15:34:13 -0500

    Updated shiny graph themes

--- app.R
+++ app.R
@@ -3,8 +3,24 @@ library(shiny)
 library(shinyWidgets)
 library(shinythemes)
 library("tidyverse")
-library(ggplot2)
-library(readr)
+
+th <- theme_minimal() + 
+  theme(
+    rect = element_blank(), 
+    panel.grid = element_blank(),
+    panel.background = element_rect(fill = "#4d5d6c", color = "#4d5d6c"),
+    plot.background = element_rect(fill = "#2b3e4f", color = "#2b3e4f"),
+    axis.text = element_text(color = "#ffffff"),
+    axis.title = element_text(size = 16, color = "#ffffff"),
+    strip.text = element_text(color = "#ffffff"), 
+    legend.text = element_text(color = "#ffffff"),
+    legend.title = element_text(color = "#ffffff"), 
+    legend.position = "bottom", 
+    panel.spacing.x = unit(10, "mm")
+  ) 
+theme_set(th)
+
+color_theme = "Paired"
 
 # Read in data 
 clean_deaths <- readr::read_csv("data//deaths_with_AllCountries.csv")
@@ -22,17 +38,18 @@ create_Percent_Graph <- function(selectedCountries, selectedYearRange) {
            Percent_Deaths = 100*Death_Numbers/Total_Deaths, 
            Year = factor(Year)) %>%
     ungroup() %>%
+    
     ggplot() +
     geom_col(position = "dodge",
              aes(x = Percent_Deaths,
                  y = reorder(Causes_name, Percent_Deaths),
                  fill=Entity)) +
     facet_wrap(~Year) +
-    theme_minimal() +
-    theme(legend.position="bottom") +
     labs(x="Percent of Deaths", y = "Cause of Death") +
     guides(fill=guide_legend(title="Country")) + 
-    scale_x_continuous(expand = c(0, 0))
+    scale_x_continuous(expand = c(0, 0))+ 
+    scale_fill_brewer(palette = color_theme) +
+    th
   return(p)
 }
 
@@ -41,17 +58,18 @@ create_Number_Graph <- function(selectedCountries, selectedYearRange) {
     filter(Entity %in% selectedCountries, 
            Year %in% selectedYearRange) %>%
     mutate(Year = factor(Year)) %>%
+    
     ggplot() +
     geom_col(position = "dodge",
              aes(x = Death_Numbers,
                  y = reorder(Causes_name, Death_Numbers),
                  fill=Entity)) + 
     facet_wrap(~Year) +
-    theme_minimal() +
-    theme(legend.position="bottom") +
     labs(x="Number of Deaths", y = "Cause of Death") +
     guides(fill=guide_legend(title="Country")) + 
-    scale_x_continuous(expand = c(0, 0))
+    scale_x_continuous(expand = c(0, 0)) + 
+    scale_color_brewer(palette = color_theme) +
+    th
   return(p)
 }
 
@@ -59,13 +77,14 @@ create_Number_Causes_Graph <- function(selectedCountries, selectedCauses) {
   p <- clean_deaths %>%
     filter(Entity %in% selectedCountries, 
            Causes_name %in% selectedCauses) %>%
+    
     ggplot() + 
     geom_line(aes(x = Year, y = Death_Numbers, col = Entity)) + 
     facet_wrap(~Causes_name) + 
-    theme_minimal() +
-    theme(legend.position="bottom") +
     labs(y="Number of Deaths", x = "Year") +
-    guides(col=guide_legend(title="Country"))
+    guides(col=guide_legend(title="Country")) + 
+    scale_color_brewer(palette = color_theme) +
+    th
   return(p)
 }
 
@@ -77,13 +96,14 @@ create_Percent_Causes_Graph <- function(selectedCountries, selectedCauses) {
     mutate(Total_Deaths = sum(Death_Numbers, na.rm = TRUE), 
            Percent_Deaths = 100*Death_Numbers/Total_Deaths) %>%
     ungroup() %>%
+    
     ggplot() + 
     geom_line(aes(x = Year, y = Percent_Deaths, col = Entity)) + 
     facet_wrap(~Causes_name) + 
-    theme_minimal() +
-    theme(legend.position="bottom") +
     labs(y="Percent of Deaths", x = "Year") +
-    guides(col=guide_legend(title="Country"))
+    guides(col=guide_legend(title="Country")) + 
+    scale_color_brewer(palette = color_theme) +
+    th
   return(p)
 }
 
@@ -181,7 +201,7 @@ server = function(input, output) {
     }else{
       create_Percent_Graph(input$country, input$yearRange)
     }
-  })
+  }, height = 600)
   
   output$causeChart <- renderPlot({
     if(input$yStyle2){
@@ -189,7 +209,7 @@ server = function(input, output) {
     }else{
       create_Percent_Causes_Graph(input$country2, input$causes)
     }
-  })
+  }, height = 600)
 }
 
 shinyApp(ui = ui, server = server)
