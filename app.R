506343e1f7125da9aec47530cf92500bc482ea34
Celina Ough <ough@wisc.edu>
2022-05-02 18:58:52 -0500

    Added text to overview

--- app.R
+++ app.R
@@ -154,14 +154,20 @@ worldmap <- function(year, Cause, region){
   mapParams$legendText 
 }
 
-# TEXT 
-motivation <- "This interface was created to answer that question. We wanted to create a general interface so that every user could use it, regardless of their country. 
-If a reader wants to know what causes of death are more prevalent in their country, they can focus on adjusting their lifestyle and risk factors to prevent those causes of death. 
-Additionally, users can look at the temporal trends of specific causes of deaths throughout each country."
-DeathsByCountry <- "The Deaths by Country tab allows users to investigate how the top causes of death in selected countries and years."
-DeathsByCause <- "The Deaths by Country tab allows users to investigate trendlines of selected causes of death in selected countries between 1990 and 2019. "
-WorldMapDesc <- "The World Map tab visualizes the countries with the most deaths from the selected cause of death."
+# Overview Text 
+keyQuestion <- "How does the cause of death vary and change across regions, countries, and years?"
+introduction <- "This interface was created to answer that question. We wanted to create a general interface so that every user could use it, regardless of their country. If a reader wants to know what causes of death are more prevalent in their country, they can focus on adjusting their lifestyle and risk factors to prevent those causes of death. Additionally, users can look at the temporal trends of specific causes of deaths throughout each country."
+WorldMapCard <- "The World Map tab allows you to see which countries are most impacted by each cause of death. You can select a cause of death and a year and a heat map will be generate showing which countries have the highest percentage of deaths for each cause. \nYou can use this data see where different causes are most common or to follow the spread of an epidemic across the years as it spreads to neighboring countries."
+CauseByTimeCard <- "The Deaths by Cause tab allows you to see how different disease trend through the years. You can select countries and causes and see how the percentage (or number) of deaths have increased or decreased through the years. You can use this to see spikes in deaths or to compare the number of deaths in different countries or between different causes. "
+DeathsByCountryCard <- "The Deaths by Country tab allows you to view a general overview of the most common causes of deaths in each country. It allows you to select countries and years and it displays the percent (or number) of deaths for twenty most common deaths. "
 
+# Where to Start
+commonWorldMap <- "The world map is very good at seeing what causes of death were most common in different regions. The most common cause of death for every year between 1990 and 2019 was Cardiovascular Disease, followed by Neoplasms. Try looking at those diseases to see if they take a higher percentage of deaths on some regions more than others."
+regionalWorldMap <- "Try looking at Tuberculosis. The map shows that it is most prevelant in Central Africa and South/Southeast Asia. However, if you change you year you can see that, while it is concentrated in Somolia and the Central African Republic right now, it used to be a much bigger issue in Southeast Asia. \nThe TB vaccine was created in 1921 so the disease hasn't spread much to other countries even with immigration and has instead slowly decreased in the regions it is present in."
+HIV1 <- "HIV/AIDS is currently very common in South Africa. However, if you look back into the early 1990s you can see that it used to much bigger in other countries like Uganda and Botswana which implies that it spread from those countries to South Africa."
+HIV2 <- "If you examine the HIV/AIDS trend in the Cause by Time tab you can see more information on how it spread. By selecting the countries we saw in the world map (South Africa, Botswana, Tanzania, Uganda, Zambia, Zimbabwe) we can see that the Uganda's percent deaths was almost twice that of other countries in 1990. However, other countries quickly overtook Uganda until they too, declined, leaving South Africa as the current epicenter. "
+HIV3 <- "However, you may remember that the US had an HIV/AIDS epidemic in the early 90s. Where is that on the graph? Since the population of the US is much larger than that of other countries the percent of people who died from HIV is much lower than that of countries in Africa. However, if we change the y-axis to show deaths by numbers we can see that while, it does have a higher number of deaths in the early 90s than countries like Zambia, Botswanwa the number has nothing on the number of people dying in South Africa right now. "
+generalDeathsByCountry <- "If you examine two countries like the United States and South Africa you can see that they have share the most common causes of death. However, you can also see that the causes of deaths in the US is highly concentrated on Cardiovascular Diseases and Neoplasms whereas, the deaths in South Africa are more evenly distributed amongst the top twenty causes."
 
 # Shiny App 
 ui = fluidPage(
@@ -171,19 +177,69 @@ ui = fluidPage(
   navbarPage("Causes of Death",
              
              # Tab 1: Overview
-             tabPanel("Overview", 
+             tabPanel("Overview"
+                      , 
                titlePanel("Project Overview"),
-               h3("How does the cause of death vary and change across countries and years?"), 
-               p(motivation), 
+               h3(keyQuestion), 
+               p(introduction), 
                
                hr(), 
                
-               h4("Deaths by Country"), 
-               p(DeathsByCountry), 
-               h4("Deaths by Cause"), 
-               p(DeathsByCause), 
-               h4("World Map"), 
-               p(WorldMapDesc), 
+               fluidRow(
+                 column(width = 4,
+                        div(class = "card border-primary mb-3",
+                            h3("World Map"), 
+                            div(class = "card-body", 
+                                p(class = "card-text", WorldMapCard)
+                                ), 
+                            )
+                        ), 
+                 
+                 column(width = 4,
+                        div(class = "card border-primary mb-3",
+                            h3("Cause By Time"), 
+                            div(class = "card-body", 
+                                p(class = "card-text", CauseByTimeCard)
+                            ), 
+                        )
+                 ), 
+                 
+                 column(width = 4,
+                        div(class = "card border-primary mb-3", 
+                            h3("Deaths By Country"), 
+                            div(class = "card-body", 
+                                p(class = "card-text", DeathsByCountryCard)
+                            ), 
+                        )
+                 )
+               ), 
+               
+               hr(), 
+               
+               h2("Where to Start"), 
+               h3("Examining Maps"),
+               fluidRow(
+                 column(width = 4, 
+                        p(commonWorldMap), 
+                        ), 
+                 column(width = 4, 
+                        p(regionalWorldMap)
+                        ), 
+                 column(width = 4, 
+                        p(generalDeathsByCountry))
+               ), 
+               
+               h3("Examining One Cause of Death: HIV/AIDS"), 
+               fluidRow(
+                 column(width = 4, 
+                        p(HIV1), 
+                 ), 
+                 column(width = 4, 
+                        p(HIV2)
+                 ), 
+                 column(width = 4, 
+                        p(HIV3))
+               ), 
                
                hr(), 
                
@@ -236,8 +292,8 @@ ui = fluidPage(
                       fluidRow( width = 11, plotOutput("map"))
              ), 
              
-             # Tab 3: Deaths by Cause
-             tabPanel("Deaths by Cause",
+             # Tab 3: Cause by Time
+             tabPanel("Cause by Time",
                       fluidRow(
                         
                         # Select Countries 
